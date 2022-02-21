a5e076324bd4707e477b9ecfd79a98904522cdbd
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-02-21 17:19:03 +0530

    Update dbconnection.php

--- Vehicle parking management System project/vpms/includes/dbconnection.php
+++ Vehicle parking management System project/vpms/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "vpmsdb");
+$con=mysqli_connect("rdsnewfinal.cxnn8acptt6e.us-east-1.rds.amazonaws.com:3306", "admin", "muni1234", "vpmss");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
